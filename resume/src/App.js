a6d52d49635fa2d053123e054c09da4dcbb449c2
Sanjay Sarang C K <126747194+sarang000@users.noreply.github.com>
2023-03-31 11:24:22 +0530

    Add files via upload

---
+++ resume/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+function App() {
+  return (
+    <div class="Appp">
+      <header class="pp-header">
+        <h1>About Me</h1>
+        <p>My name is Sanjay Sarang C K</p>
+      </header>
+<br></br>
+      <section class="aaa">
+        <h2>Projects</h2>
+        <ol>
+          <li>
+            <h3>Project 1</h3>
+            <p>Music note detection using matlab</p>
+          </li>
+          <br></br>
+          <li>
+            <h3>Project 2</h3>
+            <p>automatic pet-feeder</p>
+          </li>
+          <br></br>
+          <li>
+            <h3>Project 3</h3>
+            <p>accident prevntion system using nrf module</p>
+          </li>
+        </ol>
+      </section>
+      <br></br>
+      <br></br>
+      <section class="bbb">
+        <h2>Skills</h2>
+        <ul>
+          <li>ReactJS</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>Rust</li>
+          <li>Scala</li>
+          <li>Swift</li>
+          <li>C and C++</li>
+          <li>Kotlin</li>
+          <li>Go</li>
+          <li>Matlab</li>
+          <li>R</li>
+        </ul>
+      </section>
+      <br></br>
+      <br></br>
+      <section class="resume">
+        <h2>Resume</h2>&nbsp;&nbsp;
+        <a href="/path/to/resume.pdf" target="_blank">Download My Resume</a>
+      </section>
+      <br></br>
+      <br></br>
+      <section class="ddd">
+        <h2>Contact</h2>
+        <p>Feel free to contact me at sanjaysarang@mail.com</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
